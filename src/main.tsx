25cdf5462f778f2e672fd0b58447b7adbfe99af3
Alesh <94743489+aleshp@users.noreply.github.com>
2025-12-25 23:52:40 +0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// === ВАЖНОЕ ИСПРАВЛЕНИЕ ДЛЯ MATHQUILL ===
-// 1. Импортируем jQuery
-import $ from 'jquery';
-
-// 2. Делаем его глобальным (MathQuill ищет его именно так)
-// @ts-ignore
-window.jQuery = $;
-// @ts-ignore
-window.$ = $;
-// @ts-ignore
-window.global = window; // Страховка для первой ошибки
-// ========================================
+// jQuery больше НЕ НУЖЕН. Удалили все строки с ним.
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
